861525226dab02af123f40dee4e43e4c0e51c929
Viktor Likin <viktor.likin@gmail.com>
2016-02-15 11:14:32 +0200

    The 3-d article has been procesed.

---
+++ Model/Post/Collection.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Vlikin\Blog\Model\ResourceModel\Post;
+
+class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
+{
+    /**
+     * @var string
+     */
+    protected $_idFieldName = 'post_id';
+
+    /**
+     * Define resource model
+     *
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->_init('Vlikin\Blog\Model\Post', 'Vlikin\Blog\Model\ResourceModel\Post');
+    }
+
+}
